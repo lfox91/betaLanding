e0edd98b2fe017b90c8a795a164ecdfed5478a3a
Lea Fox <lsfox91@gmail.com>
2016-06-25 12:54:51 -0700

    update

--- server.js
+++ server.js
@@ -1,25 +1,26 @@
-const express = require('express');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const add = require('./controller/email.js');
-const validate = require('./middleware/validate.js');
-// var ip_addr = process.env.OPENSHIFT_DIY_IP   || '127.0.0.1';
-const port = process.env.PORT || 3000; 
-const app = express();
-//
-// // default to a 'localhost' configuration:
-// var connection_string = '127.0.0.1:27017/centerstage';
-// // if OPENSHIFT env variables are present, use the available connection info:
-// if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
-//   connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-//   process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-//   process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-//   process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-//   process.env.OPENSHIFT_APP_NAME;
-// }
-//
-// mongoose.connect(connection_string);
+const  express      = require('express'),
+       morgan       = require('morgan'),
+       bodyParser   = require('body-parser'),
+       mongoose     = require('mongoose'),
+       add          = require('./controller/email.js'),
+       validate     = require('./middleware/validate.js'),
+       contentTypes = require('./utils/content-types'),
+       sysInfo      = require('./utils/sys-info'),
+       port         = process.env.PORT || 3000, 
+       app          = express();
+
+// default to a 'localhost': configuration:
+var connection_string = '127.0.0.1:27017/centerstage';
+// if OPENSHIFT env variables are present, use the available connection info:
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
+  connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+  process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+  process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+  process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+  process.env.OPENSHIFT_APP_NAME;
+}
+
+mongoose.connect(connection_string);
 
 app.use(express.static(__dirname +'/diy'));
 app.use(morgan('dev'));                                         // log with Morgan
@@ -31,12 +32,16 @@ app.use(bodyParser.text());                                     // allows bodyPa
 //   res.send('/diy/index.html');
 // })
 
+app.get('/health', function(req, res){
+  res.writeHead(200);
+  res.end();
+} );
+
 app.post('/save', validate, add, function(req, res){
   var email = req.body.email;
   res.send(email);
 } );
 
-app.listen(port, function() {
-  console.log('%s: Node server started on port: %d ...',
-     Date(Date.now() ), port);
+app.listen(env.NODE_PORT || 3000, env.NODE_IP || 'localhost', function() {
+  console.log(Date.now()+` | Application worker ${process.pid} started...`);
 });
