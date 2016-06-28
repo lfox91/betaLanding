9bd5ad3c59f0677d3d4f160407860c65f27d51e4
Lea Fox <lsfox91@gmail.com>
2016-06-28 18:49:54 -0400

    rm es6 syntax

--- model/schema.js
+++ model/schema.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
 /**
  * Email Schema
  */
 
-const EmailSchema = new Schema({
+var EmailSchema = new Schema({
   email: { type: String, default: '' },
 });
-let emailModel = mongoose.model('Email', EmailSchema);
+var emailModel = mongoose.model('Email', EmailSchema);
 exports.EmailModel = emailModel;
