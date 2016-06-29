301eea700e59b455f707acf3440e6218aaa45aba
Lea Fox <lsfox91@gmail.com>
2016-06-28 22:44:48 -0400

    email.js syn

--- controller/email.js
+++ controller/email.js
@@ -3,11 +3,12 @@ var emailModel = require("../model/schema.js");
 module.exports = function (req, res, next) {
   var email = new emailModel ({ email: req.body.email });
 
-  email.save( function( err, email ) {
-    if (err) return console.error(err);
-    else console.log(req.body.email + "saved to database");
+  email.save( function( err, emailData ) {
+    if (err) console.error(err);
+    else{
+      console.log(emailData + "saved to database");
+      next();
+    }
   });
-
-  next();
 }
 
